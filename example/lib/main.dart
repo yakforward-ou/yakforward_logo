dc61af006ba9549ac10ce970951c248e66a310c7
Francesco Iapicca <iapicca@gmail.com>
2023-08-08 16:55:53 +0200

    update to dart3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:yakforward_logo/yakforward_logo.dart';
 
 void main() => runApp(
-      MaterialApp(
+      const MaterialApp(
         home: Material(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
+            children: [
               Center(
                 child: Icon(
                   YakforwardLogo.icon,
